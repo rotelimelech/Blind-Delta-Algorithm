9f5c5cc38bc50a189bda685cd6ceecaac4b02128
Katarzyna Rachwal <kasiarachwal@gmail.com>
2024-04-25 14:59:02 -0400

    adding cx freeze config creation to deployment since it shares executable description and name with action

--- boinc/generated_boinc_setup_template.py
+++ boinc/generated_boinc_setup_template.py
@@ -1,5 +1,6 @@
 """
-This file is the build configuration for cx_Freeze to generate an executable binary across platforms
+This file is the template build configuration for cx_Freeze to generate an executable binary across platforms
+Parameters EXECUTABLE and DESCRIPTION come from the GitHub workflow
 """
 from cx_Freeze import setup, Executable
 
@@ -11,11 +12,11 @@ build_options = {'packages': [],
 base = 'console'
 
 executables = [
-    Executable('execute_from_json.py', base='console', target_name='BlindDeltaAlgo')
+    Executable('execute_from_json.py', base='console', target_name='EXECUTABLE')
 ]
 
-setup(name='BlindDeltaAlgo',
+setup(name='EXECUTABLE',
       version='0.1',
-      description='Blind Delta Algorithm',
+      description='APP_DESCRIPTION',
       options={'build_exe': build_options},
       executables=executables)
