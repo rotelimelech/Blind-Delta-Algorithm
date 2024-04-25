544b62f9223887136120e85436905243c61a1133
Katarzyna Rachwal <kasiarachwal@gmail.com>
2024-04-25 16:02:19 -0400

    debugging refactor

--- boinc/freeze_config_template.py
+++ boinc/freeze_config_template.py
@@ -1,6 +1,6 @@
 """
 This file is the template build configuration for cx_Freeze to generate an executable binary across platforms
-Parameters EXECUTABLE and DESCRIPTION come from the GitHub workflow
+Parameters EXECUTABLE and APP_DESCRIPTION come from the GitHub workflow
 """
 from cx_Freeze import setup, Executable
 
@@ -12,11 +12,11 @@ build_options = {'packages': [],
 base = 'console'
 
 executables = [
-    Executable('execute_from_json.py', base='console', target_name='EXECUTABLE')
+    Executable('execute_from_json.py', base='console', target_name='{EXECUTABLE}')
 ]
 
-setup(name='EXECUTABLE',
+setup(name='{EXECUTABLE}',
       version='0.1',
-      description='APP_DESCRIPTION',
+      description='{APP_DESCRIPTION}',
       options={'build_exe': build_options},
       executables=executables)
